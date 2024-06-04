09d6f80ae506db544c5019e053df26daf2a5c408
alanakov <angelikovalski@gmail.com>
2024-06-04 12:43:14 -0300

    footer e header do indexphp com scss

--- index.php
+++ index.php
@@ -1,47 +1,82 @@
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GameHub</title>
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/style.css">
 </head>
+
 <body>
     <header>
-        <h1>Teste</h1>
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <a class="navbar-brand d-flex" href="#">
+                    <img src="imagens/LogoGameHub.png" alt="GameHub">
+                </a>
+                <p class="mt-3 d-none d-md-block">GameHub</p>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse text-white" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">HOME</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">CONTATO</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">MEMBROS</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     </header>
-
 
     <main>
 
+        <?php
+        if (isset($_GET["param"])) {
+            $param = $_GET["param"];
+            $p = explode("/", $param);
+        }
+
+        $page = $p[0] ?? "home";
+        $jogo = $p[1] ?? NULL;
+
+        if ($page == "jogo") {
+            $pagina == "jogo/{$jogo}.php";
+        } else {
+            $pagina = "paginas/{$page}.php";
+        }
+
+        if (file_exists($pagina)) {
+            include $pagina;
+        } else {
+            include "paginas/erro.php";
+        }
+        ?>
     </main>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
 
     <footer>
-
+        <div class="footer text-center p-3">
+            <a href="#">
+                <img src="imagens/LogoGameHubBranca.png" alt="GameHub">
+            </a>
+            <h4 class="fs-6 text-white p-2">Alana Kovalski de Angeli ©</h4>
+        </div>
     </footer>
 </body>
-</html> -->
 
-<?php
-
-    if (isset($_GET["param"])) {
-    $param = $_GET["param"];
-    $p = explode("/", $param);
-    }
-
-    $page = $p[0] ?? "home";
-    $jogo = $p[1] ?? NULL;
-
-    if ($page == "jogo") {
-        $pagina == "jogo/{$jogo}.php";
-    } else {
-        $pagina = "paginas/{$page}.php";
-    }
-
-    if (file_exists($pagina)) {
-        include $pagina;
-    } else {
-        include "paginas/erro.php";
-    }
+</html>
